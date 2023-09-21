19560d0eca290c19bd03a980e486a9901d0ed0a3
Phyllipe Soares <89877375+Phyllipesa@users.noreply.github.com>
2023-09-21 16:07:51 -0300

    Update MatchRepository.ts
    Adição da ITeamPersistence.

--- app/backend/src/domain/repository/MatchRepository.ts
+++ app/backend/src/domain/repository/MatchRepository.ts
@@ -1,8 +1,12 @@
 import { IMatch, IMatchWithTeams } from '../entities/IMatch';
 import IMatchPersistence from './IMatchPersistence';
+import ITeamPersistence from './ITeamPersistence';
 
 class MatchRepository {
-  constructor(private iPersistence: IMatchPersistence) {}
+  constructor(
+    private iPersistence: IMatchPersistence,
+    private iPersistence2: ITeamPersistence,
+  ) {}
 
   public getAll = async (entity: any): Promise<IMatchWithTeams[]> => this
     .iPersistence.getAll(entity);
@@ -15,6 +19,9 @@ class MatchRepository {
 
   public createMatch = async (entity: any): Promise<IMatch> => this
     .iPersistence.createMatch(entity);
+
+  public verifyTeams = async (entity: any): Promise<IMatch> => this
+    .iPersistence2.getById(entity);
 }
 
 export default MatchRepository;
