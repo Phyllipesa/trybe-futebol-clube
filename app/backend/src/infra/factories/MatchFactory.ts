8277040b12f037652e4275dbc3684b83e4c1a61b
Phyllipe Soares <89877375+Phyllipesa@users.noreply.github.com>
2023-09-21 16:02:37 -0300

    Update MatchFactory.ts
    Adicionando a teamPersistence e teamRepository, para usar a teamRepository dentro da matchService afim de tornar possivel a validação dos Id's fornecidos para a criação de uma partida.

--- app/backend/src/infra/factories/MatchFactory.ts
+++ app/backend/src/infra/factories/MatchFactory.ts
@@ -3,11 +3,18 @@ import MatchController from '../controllers/MatchController';
 import MatchRepository from '../../domain/repository/MatchRepository';
 import IMatchPersistence from '../../domain/repository/IMatchPersistence';
 import MatchPersistenceMySQL from '../persistence/MatchORM';
-// import TeamRepository from '../../domain/repository/TeamRepository';
+import ITeamPersistence from '../../domain/repository/ITeamPersistence';
+import TeamPersistenceMySQL from '../persistence/TeamORM';
+import TeamRepository from '../../domain/repository/TeamRepository';
 
+const teamPersistence: ITeamPersistence = new TeamPersistenceMySQL();
 const matchPersistence: IMatchPersistence = new MatchPersistenceMySQL();
-const matchRepository = new MatchRepository(matchPersistence);
-const matchService = new MatchService(matchRepository);
+
+const teamRepository = new TeamRepository(teamPersistence);
+const matchRepository = new MatchRepository(matchPersistence, teamPersistence);
+
+const matchService = new MatchService(matchRepository, teamRepository);
 const matchController = new MatchController(matchService);
 
 export default matchController;
+
